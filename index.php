20d509615f8235316aecc7decfe0a24e227954ed
Mehdi Achour <didou@php.net>
2009-03-29 20:21:36 +0000

    Get rid of inline html

--- index.php
+++ index.php
@@ -1,71 +1,60 @@
 <?php
 session_start();
 
+// Log the user in if needed
 if (!isset($_SESSION['userID'])) {
     echo headerTemplate();
-?>
-  <link type="text/css" rel="stylesheet" href="extjs/resources/css/ext-all.css" />
-  <link type="text/css" rel="stylesheet" href="themes/style.css" />
-  <!-- ExtJs Javascript core files -->
-  <script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Loading Core API...';</script>
-  <script type="text/javascript" src="extjs/adapter/ext/ext-base.js"></script>
-  <script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Loading UI Components...';</script>
-  <script type="text/javascript" src="extjs/ext-all.js"></script>
-  <script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Initializing...';</script>
-  <script type="text/javascript" src="js/login.js"></script>
-<?php
+    echo cssLoadTemplate('extjs/resources/css/ext-all.css');
+    echo cssLoadTemplate('themes/style.css');
+    echo jsCallTemplate('document.getElementById("loading-msg").innerHTML = "Loading Core API...";');
+    echo jsLoadTemplate('extjs/adapter/ext/ext-base.js');
+    echo jsCallTemplate('document.getElementById("loading-msg").innerHTML = "Loading UI Components...";');
+    echo jsLoadTemplate('extjs/ext-all.js');
+    echo jsCallTemplate('document.getElementById("loading-msg").innerHTML = "Initializing...";');
+    echo jsLoadTemplate('js/login.js');
     echo footerTemplate();
     exit;
 }
 
 echo headerTemplate();
+echo cssLoadTemplate('extjs/resources/css/ext-all.css');
+echo cssLoadTemplate('extjs/resources/css/xtheme-default.css', 'appTheme');
+// Ext.ux Css files
+echo cssLoadTemplate('js/ux/GridSummary/Ext.ux.grid.GridSummary.css');
+echo cssLoadTemplate('js/ux/CheckTreePanel/Ext.ux.tree.CheckTreePanel.css');
+echo cssLoadTemplate('themes/style.css');
+// ExtJs Javascript core files
+echo jsCallTemplate('document.getElementById("loading-msg").innerHTML = "Loading Core API...";');
+echo jsLoadTemplate('extjs/adapter/ext/ext-base.js');
+echo jsCallTemplate('document.getElementById("loading-msg").innerHTML = "Loading UI Components...";');
+echo jsLoadTemplate('extjs/ext-all.js');
+// Ext.ux Javascript files
+echo jsCallTemplate('document.getElementById("loading-msg").innerHTML = "Initializing...";');
+echo jsLoadTemplate('js/ux/GridSummary/Ext.ux.grid.GridSummary.js');
+echo jsLoadTemplate('js/ux/miframe1_2/miframe.js');
+echo jsLoadTemplate('js/ux/md5/md5.js');
+echo jsLoadTemplate('js/ux/codemirror/js/codemirror.js');
+echo jsLoadTemplate('js/ux/Ext.ux.CodeMirror.js');
+echo jsLoadTemplate('js/ux/CheckTreePanel/Ext.ux.tree.CheckTreePanel.js');
+echo jsLoadTemplate('js/main_override.js');
+echo jsLoadTemplate('js/main.js');
 
-?>
-  <link type="text/css" rel="stylesheet" href="extjs/resources/css/ext-all.css" />
-  <link type="text/css" rel="stylesheet" href="extjs/resources/css/xtheme-default.css" id="appTheme" />
-
-  <!-- Ext.ux Css files -->
-  <link type="text/css" rel="stylesheet" href="js/ux/GridSummary/Ext.ux.grid.GridSummary.css" />
-  <link type="text/css" rel="stylesheet" href="js/ux/CheckTreePanel/Ext.ux.tree.CheckTreePanel.css" />
-  <link type="text/css" rel="stylesheet" href="themes/style.css" />
-
-  <!-- ExtJs Javascript core files -->
-  <script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Loading Core API...';</script>
-  <script type="text/javascript" src="extjs/adapter/ext/ext-base.js"></script>
-
-  <script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Loading UI Components...';</script>
-  <script type="text/javascript" src="extjs/ext-all.js"></script>
-
-  <!-- Ext.ux Javascript files -->
-  <script type="text/javascript">document.getElementById('loading-msg').innerHTML = 'Initializing...';</script>
-  <script type="text/javascript" src="js/ux/GridSummary/Ext.ux.grid.GridSummary.js"></script>
-
-  <script type="text/javascript" src="js/ux/miframe1_2/miframe.js"></script>
-  <script type="text/javascript" src="js/ux/md5/md5.js"></script>
-
-  <script type="text/javascript" src="js/ux/codemirror/js/codemirror.js"></script>
-  <script type="text/javascript" src="js/ux/Ext.ux.CodeMirror.js"></script>
-
-  <script type="text/javascript" src="js/ux/md5/md5.js"></script>
-  <script type="text/javascript" src="js/ux/CheckTreePanel/Ext.ux.tree.CheckTreePanel.js"></script>
-
-  <script type="text/javascript" src="js/main_override.js"></script>
-  <script type="text/javascript" src="js/main.js"></script>
-
-  <!-- Principal -->
-<?php
-
-echo '  <script type="text/javascript">' . "\n";
 if (isset($_SESSION['directAccess']) && is_object($_SESSION['directAccess'])) {
-    echo 'var directAccess = {"lang":"'.$_SESSION['directAccess']->lang.'", "path":"'.$_SESSION['directAccess']->path.'", "name":"'.$_SESSION['directAccess']->name.'"}';
+    $directAccess = 'var directAccess = {"lang":"'.$_SESSION['directAccess']->lang.'", "path":"'.$_SESSION['directAccess']->path.'", "name":"'.$_SESSION['directAccess']->name.'"}';
     $_SESSION['directAccess'] = '';
 } else {
-    echo 'var directAccess = false;';
+    $directAccess = 'var directAccess = false;';
 }
-echo '  </script>' . "\n";
+echo jsCallTemplate($directAccess);
 
 echo footerTemplate();
 
+/**
+ * Gets the tool header template
+ *
+ * @param string $title The HTML page title. Default to the tool name
+ * @return string
+ */
 function headerTemplate($title = 'PHP Documentation Online Editor')
 {
     return <<<EOD
@@ -81,13 +70,57 @@ function headerTemplate($title = 'PHP Documentation Online Editor')
   <div id="loading-mask" style=""></div>
   <div id="loading">
    <div class="loading-indicator">
-    <img src="img/php.png" width="95" height="51" style="margin-right:8px;float:left;vertical-align:top;"/>PhpDocumentation Online Editor<br />
+    <img src="img/php.png" class="loading-php-logo" alt="PHP" />PhpDocumentation Online Editor<br/>
     <span id="loading-msg">Loading styles and images...</span>
    </div>
   </div>
+
 EOD;
 }
 
+/**
+ * Returns a javascript inline call
+ *
+ * @param string $script The javascript code
+ * @return string The script element
+ */
+function jsCallTemplate($script)
+{
+    return sprintf('  <script type="text/javascript">%s</script>', $script) . "\n";
+}
+
+/**
+ * Returns a javascript script loading element
+ *
+ * @param string $src The script url
+ * @return string The script element
+ */
+function jsLoadTemplate($src)
+{
+    return sprintf('  <script type="text/javascript" src="%s"></script>', $src) . "\n";
+}
+
+
+/**
+ * Returns a stylesheet loading element
+ *
+ * @param string $href The stylesheet url
+ * @param string $id An optional id for the HTML element
+ * @return string The link element
+ */
+function cssLoadTemplate($href, $id = '')
+{
+    if ($id) {
+        $id = 'id="' . $id . '" ';
+    }
+    return sprintf('  <link type="text/css" rel="stylesheet" href="%s" %s/>', $href, $id) . "\n";
+}
+
+/**
+ * Returns the tool footer template
+ *
+ * @return string
+ */
 function footerTemplate()
 {
     return <<<EOD
