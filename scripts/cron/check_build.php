7940c8f04994799e9308498ee040dbdc02eef50f
Yannick Torres <yannick@php.net>
2009-11-14 20:57:28 +0000

    cvs => vcs

--- scripts/cron/check_build.php
+++ scripts/cron/check_build.php
@@ -4,7 +4,7 @@
 * This script is intended to be placed in a cronjob.
 * It must be run every Thursday, at 01hOO for example.
 * On Unix, you can use crontab -e and place this :
-* 00 01 * * 4 /usr/bin/php /path/to/your/cvs/dir/doc-editor/scripts/cron/check_build.php
+* 00 01 * * 4 /usr/bin/php /path/to/your/vcs/dir/doc-editor/scripts/cron/check_build.php
 ****/
 
 require_once dirname(__FILE__) . '/../../php/conf.inc.php';
