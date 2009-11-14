4bd30753af7f9122e53bd07471b62959c8eb03fe
Yannick Torres <yannick@php.net>
2009-11-14 21:02:27 +0000

    cvs => vcs

--- scripts/cron/check_build.php
+++ scripts/cron/check_build.php
@@ -15,7 +15,7 @@ require_once dirname(__FILE__) . '/../../php/RepositoryManager.php';
 // For all language, we check the build
 foreach (RepositoryManager::getInstance()->availableLang as $lang) {
 
-    $cmd = 'cd '.DOC_EDITOR_CVS_PATH.';/usr/bin/php configure.php --with-lang='.$lang.' --disable-segfault-error';
+    $cmd = 'cd '.DOC_EDITOR_VCS_PATH.';/usr/bin/php configure.php --with-lang='.$lang.' --disable-segfault-error';
     $output = array();
     exec($cmd, $output);
 
--- scripts/cron/update_data.php
+++ scripts/cron/update_data.php
@@ -4,19 +4,18 @@
 * This script is intended to be placed in a cronjob.
 * It must be run every day, at 00hOO for example.
 * On Unix, you can use crontab -e and place this :
-* 00 00 * * * /usr/bin/php /path/to/your/cvs/dir/doc-editor/scripts/cron/update_data.php
+* 00 00 * * * /usr/bin/php /path/to/your/vcs/dir/doc-editor/scripts/cron/update_data.php
 ****/
-
 
 require_once dirname(__FILE__) . '/../../php/conf.inc.php';
 require_once dirname(__FILE__) . '/../../php/RepositoryManager.php';
 
 $rm = RepositoryManager::getInstance();
 
-// Cvs update
+// VCS update
 $rm->updateRepository();
 
-// After update the repo, we need to chmod all file to be able to save it again as www server user
+// After update the repo, we need to chmod all file to be able to save it again as www server user & group
 $cmd = "cd ".DOC_EDITOR_VCS_PATH."; chmod -R 775 ./; chown -R  ".DOC_EDITOR_WWW_USER.":".DOC_EDITOR_WWW_GROUP." ./";
 exec($cmd);
 
