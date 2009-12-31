92c5b75352cc7bdd4b514fd1e0b5f2db81343de6
Yannick Torres <yannick@php.net>
2009-12-31 06:22:34 +0000

    Monitor onload events for iframe. We can mask now the iframe until the end of loading the content

--- js/ux/others/main_specific/Ext.ux.IFrameComponent.js
+++ js/ux/others/main_specific/Ext.ux.IFrameComponent.js
@@ -1,5 +1,29 @@
 Ext.ux.IFrameComponent = Ext.extend(Ext.BoxComponent, {
     onRender : function(ct, position){
-        this.el = ct.createChild({tag: 'iframe', id: this.id, frameBorder: 0, src: this.url});
+
+        ct.mask(
+            '<img src="themes/img/loading.gif" ' +
+                'style="vertical-align: middle;" /> '+
+            _('Loading...')
+        );
+
+        var frame    = document.createElement('iframe'),
+            callback = function(e) {
+                ct.unmask();
+            };
+
+        frame.id = this.id;
+        frame.name = this.id;
+        frame.src = this.url;
+        frame.frameBorder = 0;
+
+        this.el = ct.appendChild(frame);
+
+        if(Ext.isIE) {
+            document.frames[this.url].name = this.id;
+        }
+
+        frame[ Ext.isIE?'onreadystatechange':'onload'] = callback.createDelegate(frame);
+
     }
 });
