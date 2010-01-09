e423410f3e37ebee4aed11cee0b8dbe18b53b538
Yannick Torres <yannick@php.net>
2010-01-09 09:47:11 +0000

    Split mainPanel from main.js to MainPanel.js as a new component

--- js/main.js
+++ js/main.js
@@ -245,72 +245,6 @@ var phpDoc = function()
             }
         },
 
-        // Need confirm if we want to close a tab and the content have been modified.
-        removeTabEvent : function(tabpanel, tab)
-        {
-            var stateLang, stateEn, state, PanType = tab.id.split('-');
-
-            if ((PanType[0] === 'FE' || PanType[0] === 'FNU' || PanType[0] === 'FNR' || PanType[0] === 'PP' || PanType[0] === 'AF' || PanType[0] === 'FNT') && PanType[1] !== 'help') {
-
-                if (PanType[0] === 'FE') {
-                    stateLang = Ext.getCmp('FE-LANG-FILE-' + PanType[1]).isModified;
-                }
-                if (PanType[0] === 'FNU') {
-                    stateLang = Ext.getCmp('FNU-LANG-FILE-' + PanType[1]).isModified;
-                }
-                if (PanType[0] === 'FNR') {
-                    stateLang = Ext.getCmp('FNR-LANG-FILE-' + PanType[1]).isModified;
-                }
-
-                if (PanType[0] === 'FE') {
-                    stateEn = Ext.getCmp('FE-EN-FILE-' + PanType[1]).isModified;
-                }
-                if (PanType[0] === 'FNU') {
-                    stateEn = Ext.getCmp('FNU-EN-FILE-' + PanType[1]).isModified;
-                }
-                if (PanType[0] === 'FNR') {
-                    stateEn = Ext.getCmp('FNR-EN-FILE-' + PanType[1]).isModified;
-                }
-
-                if (PanType[0] === 'PP') {
-                    state = Ext.getCmp('PP-PATCH-FILE-' + PanType[1]).isModified;
-                }
-
-                if (PanType[0] === 'AF') {
-                    state = Ext.getCmp('AF-ALL-FILE-' + PanType[1]).isModified;
-                }
-
-                if (PanType[0] === 'FNT') {
-                    state = Ext.getCmp('FNT-TRANS-FILE-' + PanType[1]).isModified;
-                }
-
-                if (stateEn || stateLang || state) {
-                    Ext.Msg.show({
-                        scope   : this,
-                        title   : _('Confirm'),
-                        msg     : _('This file has been modified without being saved.<br/>' +
-                                    'Do you really want to close?'),
-                        buttons : Ext.Msg.YESNO,
-                        icon    : Ext.Msg.QUESTION,
-                        fn : function(btn, text)
-                        {
-                            if (btn === 'yes') {
-                                tabpanel.un('beforeremove', this.removeTabEvent, this);
-                                tabpanel.remove(tab);
-                                tabpanel.addListener('beforeremove', this.removeTabEvent, this);
-                            }
-                        }
-                    });
-                    return false;
-                } else {
-                    return true;
-                }
-            } else {
-                return true;
-            }
-
-        },
-
         drawInterface: function()
         {
             // We keel alive our session by sending a ping every minute
@@ -446,50 +380,9 @@ var phpDoc = function()
                     }]
                 }, {
                     // main panel
-                    xtype             : 'tabpanel',
+                    xtype             : 'mainpanel',
                     id                : 'main-panel',
                     region            : 'center',
-                    activeTab         : 0,
-                    enableTabScroll   : true,
-                    plugins           : new Ext.ux.TabCloseMenu(),
-                    listeners : {
-                        scope        : this,
-                        beforeremove : this.removeTabEvent,
-                        tabchange    : function(panel, tab) {
-
-                            // We do somethings only if this panel contiens a tab's navigation button
-                            if ( Ext.getCmp(tab.id + '-btn-tabRight-LANG')    ||
-                                 Ext.getCmp(tab.id + '-btn-tabRight-EN')      ||
-                                 Ext.getCmp(tab.id + '-btn-tabRight-ALL')     ||
-                                 Ext.getCmp(tab.id + '-btn-tabRight-NotInEN') ||
-                                 Ext.getCmp(tab.id + '-btn-tabRight-PATCH')   ||
-                                 Ext.getCmp(tab.id + '-btn-tabRight-TRANS')  ) {
-
-                                var currentTabId = tab.id,
-                                    tabs         = Ext.getCmp('main-panel').layout.container.items.items,
-                                    nextTabId    = false,
-                                    currentTabIndex,
-                                    i;
-
-                                for( i=0; i < tabs.length; i++ ) {
-                                    if( tabs[i].id === currentTabId ) {
-                                        currentTabIndex = i;
-                                    }
-                                }
-
-                                // Do we need to activate some button ?
-                                if( tabs[currentTabIndex + 1] ) {
-                                    if ( Ext.getCmp(tab.id + '-btn-tabRight-LANG'    ) ) { Ext.getCmp(tab.id + '-btn-tabRight-LANG'    ).enable(); }
-                                    if ( Ext.getCmp(tab.id + '-btn-tabRight-EN'      ) ) { Ext.getCmp(tab.id + '-btn-tabRight-EN'      ).enable(); }
-                                    if ( Ext.getCmp(tab.id + '-btn-tabRight-ALL'     ) ) { Ext.getCmp(tab.id + '-btn-tabRight-ALL'     ).enable(); }
-                                    if ( Ext.getCmp(tab.id + '-btn-tabRight-NotInEN' ) ) { Ext.getCmp(tab.id + '-btn-tabRight-NotInEN' ).enable(); }
-                                    if ( Ext.getCmp(tab.id + '-btn-tabRight-PATCH'   ) ) { Ext.getCmp(tab.id + '-btn-tabRight-PATCH'   ).enable(); }
-                                    if ( Ext.getCmp(tab.id + '-btn-tabRight-TRANS'   ) ) { Ext.getCmp(tab.id + '-btn-tabRight-TRANS'   ).enable(); }
-                                }
-
-                            }
-                        }
-                    },
                     items : [{
                         xtype      : 'panel',
                         id         : 'MainInfoTabPanel',
@@ -518,7 +411,6 @@ var phpDoc = function()
                                             '</p>' +
                                         '</div>' +
                                      '</div>'
-
 
                         }, {
                             xtype      : 'portal',
