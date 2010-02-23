2c9af9708fb643cffab6d8e4b5dec9616058909e
Yannick Torres <yannick@php.net>
2010-02-23 06:59:06 +0000

    Change the doctype according to this thread : http://www.extjs.com/forum/showthread.php?t=92605

--- php/html.templates.php
+++ php/html.templates.php
@@ -13,7 +13,7 @@ function headerTemplate($title = 'PHP Documentation Online Editor', $depth = 0)
 {
     $prefix = str_repeat('../', $depth);
     return <<<EOD
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
 <html>
  <head>
   <title>$title</title>
