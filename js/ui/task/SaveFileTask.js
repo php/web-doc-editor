3277a080c0c1e716371da76aac3ca3b1b3eb05c9
Yannick Torres <yannick@php.net>
2010-03-22 21:41:31 +0000

    concat the saveLangFile & SaveENFile into the save method : saveFile

--- js/ui/task/SaveFileTask.js
+++ js/ui/task/SaveFileTask.js
@@ -1,7 +1,7 @@
 Ext.namespace('ui', 'ui.task');
 
-// config - {prefix, ftype, fid, fpath, fname, storeRecord}
-ui.task.SaveENFileTask = function(config)
+// config - {prefix, ftype, fid, fpath, fname, lang, storeRecord}
+ui.task.SaveFileTask = function(config)
 {
     Ext.apply(this, config);
 
@@ -13,37 +13,50 @@ ui.task.SaveENFileTask = function(config)
         PhDOE.winForbidden();
         return;
     }
-
     XHR({
         scope  : this,
         params : {
             task        : 'saveFile',
             filePath    : this.fpath,
             fileName    : this.fname,
-            fileLang    : 'en',
+            fileLang    : this.lang,
             fileContent : Ext.getCmp(this.prefix + '-' + this.ftype +
                                         '-FILE-' + this.fid).getCode()
         },
-
         success : function(r)
         {
             var o = Ext.util.JSON.decode(r.responseText);
 
             if (this.prefix === 'FNU') {
                 // Update our store
-                this.storeRecord.set('en_revision', o.revision);
-                this.storeRecord.set('needCommitEN', true);
+                if( this.lang === 'en' ) {
+                    this.storeRecord.set('en_revision', o.revision);
+                    this.storeRecord.set('needCommitEN', true);
+                } else {
+                    this.storeRecord.set('revision', o.en_revision);
+                    this.storeRecord.set('needCommitLang', true);
+                    this.storeRecord.set('maintainer', o.maintainer);
+                }
                 this.storeRecord.commit();
             }
 
             if (this.prefix === 'FE') {
                 // Update our store
+                if( this.lang !== 'en' ) {
+                    this.storeRecord.set('maintainer', o.maintainer);
+                }
                 this.storeRecord.set('needcommit', true);
+                this.storeRecord.commit();
             }
-
+            
             if (this.prefix === 'FNR') {
                 // Update our store
+                if( this.lang !== 'en' ) {
+                    this.storeRecord.set('maintainer', o.maintainer);
+                    this.storeRecord.set('reviewed', o.reviewed);
+                }
                 this.storeRecord.set('needcommit', true);
+                this.storeRecord.commit();
             }
 
             if (this.prefix === 'AF') {
@@ -52,7 +65,7 @@ ui.task.SaveENFileTask = function(config)
 
             // Add this files into storePendingCommit
             ui.component.PendingCommitGrid.getInstance().addRecord(
-                o.id, 'en' + this.fpath, this.fname, 'update'
+                o.id, this.lang + this.fpath, this.fname, 'update'
             );
 
             // reset file
@@ -64,7 +77,14 @@ ui.task.SaveENFileTask = function(config)
                 Ext.getCmp(id_prefix + '-PANEL-' + this.fid).originTitle
             );
 
-            if (this.ftype === 'ALL' || !Ext.getCmp(this.prefix + '-LANG-FILE-' + this.fid).isModified) {
+            var cmp;
+            if( this.lang === 'en' ) {
+                cmp = Ext.getCmp(this.prefix + '-LANG-FILE-' + this.fid);
+            } else {
+                cmp = Ext.getCmp(this.prefix + '-EN-FILE-' + this.fid);
+            }
+
+            if (this.ftype === 'ALL' || !cmp.isModified) {
                 // reset tab-panel
                 Ext.getCmp(this.prefix + '-' + this.fid).setTitle(
                     Ext.getCmp(this.prefix + '-' + this.fid).originTitle
@@ -75,10 +95,8 @@ ui.task.SaveENFileTask = function(config)
             msg.hide();
 
             // Notify
-            PhDOE.notify('info', _('Document saved'), String.format(_('Document <br><br><b>{0}</b><br><br> was saved successfully !'), 'en' + this.fpath + this.fname));
-
+            PhDOE.notify('info', _('Document saved'), String.format(_('Document <br><br><b>{0}</b><br><br> was saved successfully !'), this.lang + this.fpath + this.fname));
         },
-
         failure : function(r)
         {
             var o = Ext.util.JSON.decode(r.responseText);
