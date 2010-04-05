065981bb2069eecf68280b62cd9c5fe81b52ff2e
Yannick Torres <yannick@php.net>
2010-04-05 20:05:57 +0000

    Fix App name ; Only display the first 20 lines of the log build ; Script all projects others than PHP

--- scripts/cron/check_build.php
+++ scripts/cron/check_build.php
@@ -1,10 +1,11 @@
+#!/usr/bin/env php
 <?php
 
 /***
 * This script is intended to be placed in a cronjob.
 * It must be run every Thursday, at 01hOO for example.
 * On Unix, you can use crontab -e and place this :
-* 00 01 * * 4 /usr/bin/php /path/to/your/vcs/dir/doc-editor/scripts/cron/check_build.php
+* 00 01 * * 4 /path/to/your/vcs/dir/doc-editor/scripts/cron/check_build.php
 ****/
 
 require_once dirname(__FILE__) . '/../../php/Conf.php';
@@ -19,7 +20,12 @@ $availableProject = $pm->getAvailableProject();
 
 while( list($key, $project) = each($availableProject) ) {
 
-echo "enter into ".$project['code']."\n";
+    // Actually, only PHP project support to be automatically checked for the build. Skip all others projects
+    if( $project['code'] != "php" ) {
+        continue;
+    }
+
+    echo "enter into ".$project['code']."\n";
 
     // We must delete this var to be re-generated
     unset($rm->existingLanguage);
@@ -48,6 +54,9 @@ echo "enter into ".$project['code']."\n";
             // What we must do when the build failed
             if( $return["state"] == "ko" ) {
 
+                // We put into mail only 20 lines
+                $return["logContent"] = array_slice($return["logContent"], 0, 20);
+
                 $msg = "Your documentation is broken. The build is done on Friday.
 
         Please, try to fix it *quickly*.
@@ -59,7 +68,7 @@ echo "enter into ".$project['code']."\n";
         ".implode("\n", $return["logContent"])."
 
         --
-        This email is send automatically by the PhpDocumentation Online Editor.
+        This email is send automatically by the Php Docbook Online Editor.
         ";
 
                 $to = "doc-$lang@lists.php.net";
@@ -69,6 +78,7 @@ echo "enter into ".$project['code']."\n";
                 // We send an email for this failed build
                 AccountManager::getInstance()->email($to, $subject, $msg, 'www');
 
+                echo "email send !\n";
                 // We store it into DB
                 LogManager::getInstance()->saveFailedBuild($lang, $return["logContent"]);
 
