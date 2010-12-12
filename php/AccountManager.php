595b0f1238fe582836015f10ad2a66b3b7b1a3ef
Yago Ferrer <yago@php.net>
2010-12-12 21:29:26 +0000

    Saving current language in a Cookie.

--- php/AccountManager.php
+++ php/AccountManager.php
@@ -311,6 +311,7 @@ class AccountManager
            
            setcookie("loginApp", $cookieLogin, time() + 3600*24*365, "/"); // One year ;)
            setcookie("email", $email, time() + 3600*24*365, "/");
+           setcookie("lang", $this->vcsLang, time() + 3600*24*365, "/");
 
         } elseif ($AuthReturn == 'Bad password') {
 
