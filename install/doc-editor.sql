f2abfa673d5eca766f861e32471e8e9c7c2c06e9
Yannick Torres <yannick@php.net>
2011-07-19 23:50:50 +0000

    Change type of value to LONGTEXT. Karma list don't stay in a text type ;)

--- install/doc-editor.sql
+++ install/doc-editor.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `staticValue` (
   `project` varchar(255) NOT NULL,
   `type` varchar(50) NOT NULL,
   `field` varchar(50) NOT NULL,
-  `value` text NOT NULL,
+  `value` LONGTEXT NOT NULL,
   `date` datetime NOT NULL,
   KEY `id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
