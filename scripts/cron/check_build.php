5a4a6a92bde5cf0e1002e6d2b89ec87b20214e32
Yannick Torres <yannick@php.net>
2011-10-25 15:38:50 +0000

    don't try to check the build for exclude language

--- scripts/cron/check_build.php
+++ scripts/cron/check_build.php
@@ -44,6 +44,11 @@ while( list($key, $project) = each($availableProject) ) {
     // For all language, we check the build
     foreach ($existingLanguage as $lang) {
 
+        if( in_array($lang, $dontSendEmail) ) {
+            echo "Exclude build for this language\n";
+            continue;
+        }
+
         echo "check lang : ".$lang["code"]."\n";
 
         $lang = $lang["code"];
@@ -57,9 +62,7 @@ while( list($key, $project) = each($availableProject) ) {
             // What we must do when the build failed
             if( $return["state"] == "ko" ) {
                 
-                if( ! in_array($lang, $dontSendEmail) ) {
-
-                    $msg = "Your documentation is broken. The build is done on Friday.
+                $msg = "Your documentation is broken. The build is done on Friday.
 
         Please, try to fix it *quickly*.
 
@@ -73,18 +76,17 @@ while( list($key, $project) = each($availableProject) ) {
         This email is send automatically by the Php Docbook Online Editor.
         ";
         
-                    //Usefull for language like pt_BR for example, because doc-pt_br don't exist, it's doc-pt-br
-                    $emailLang = str_replace("_", "-", strtolower($lang));
+                //Usefull for language like pt_BR for example, because doc-pt_br don't exist, it's doc-pt-br
+                $emailLang = str_replace("_", "-", strtolower($lang));
 
-                    $to = ( $lang === 'en' ) ? "phpdoc@lists.php.net" : "doc-$emailLang@lists.php.net";
+                $to = ( $lang === 'en' ) ? "phpdoc@lists.php.net" : "doc-$emailLang@lists.php.net";
 
-                    $subject = "[DOC-".strtoupper($emailLang)."] - Your documentation is broken";
+                $subject = "[DOC-".strtoupper($emailLang)."] - Your documentation is broken";
 
-                    // We send an email for this failed build
-                    AccountManager::getInstance()->email($to, $subject, $msg, $to, 'list');
+                // We send an email for this failed build
+                AccountManager::getInstance()->email($to, $subject, $msg, $to, 'list');
 
-                    echo "email send !\n";
-                }
+                echo "email send !\n";
 
                 // We store it into DB
                 LogManager::getInstance()->saveFailedBuild($lang, $return["logContent"]);
