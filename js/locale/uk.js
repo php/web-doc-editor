8ee114fad826836f6b70d6778c87c239fb3ca48c
KostyaTretyak <ktretyakgm@gmail.com>
2014-09-20 15:35:57 +0300

    Deleted nonexistent language code UA, and added a translation file uk.js

---
+++ js/locale/uk.js
@@ -0,0 +1,106 @@
+/*
+* This file must be encoded in UTF-8
+*/
+
+var i18n = new Array();
+
+// In file: ../../js/main.js
+  i18n["Refresh"] = "Оновити";
+  i18n["Forbidden"] = "Заборонено";
+  i18n["You can't do this action as anonymous user."] = "Дана дія не може бути виконана анонімним користувачем.";
+  i18n["Error"] = "Помилка";
+  i18n["File system error. Check read/write permissions under data folder."] = "Помилка файлової системи. Перевірте права на читання/запис для каталога data.";
+  i18n["Main menu"] = "Основне меню";
+  i18n["Topic:"] = "Тема:";
+  i18n["Defined by {0}, {1}"] = "Встановив {0}, {1}";
+  i18n["Files need translate"] = "Неперекладені файли";
+  i18n["Files need update"] = "Неоновлені файли";
+  i18n["Error in current translation"] = "Помилки в поточному перекладі";
+  i18n["Files need reviewed"] = "Файли для перевірки";
+  i18n["Not in EN tree"] = "Неіснуючі файли в EN дереві";
+  i18n["All files"] = "Всі файли";
+  i18n["Open the Log Message Manager"] = "Відкрити Менеджер Журнальних Записів";
+  i18n["Work in progress"] = "В роботі";
+  i18n["Patches for review"] = "Патчі для перевірки";
+  i18n["Google translation"] = "Перекладач Google";
+  i18n["Home"] = "Стартова";
+  i18n["Connected as"] = "Долучився як";
+  i18n["Connected using"] = "Долучився через";
+  i18n["anonymous"] = "Анонім";
+  i18n["Administrator"] = "Адміністратор";
+  i18n["You are a global Administrator"] = "Ви є глобальним адміністратором";
+  i18n["You are an administrator for this language"] = "Ви є адміністратором для даної мови";
+  i18n["Project: "] = "Проект: ";
+  i18n["Language: "] = "Мова: ";
+  i18n["Total number of your files"] = "Загальна кількість ваших файлів";
+  i18n["You have used characters that require the use of UTF-8 despite the XML header.<br>Please delete these characters or change the header of the XML file to UTF-8 ; i.e.:<br><br><center><i>&lt;?xml version=\"1.0\" encoding=\"utf-8\"?&gt;</i></center>"] = "Ви використали символи, для яких необхідно використовувати UTF-8, незважаючи на зазначений XML заголовок.<br>Будь-ласка, видаліть ці символи, або змініть заголовок в XML файлі на UTF-8 ; наприклад, так:<br><br><center><i>&lt;?xml version=\"1.0\" encoding=\"utf-8\"?&gt;</i></center>";
+  i18n["It seems that you have inserted tab caracters into this file. Please, replace each one by a single space.<br>Tip: You can use the \"Re-indent this entire file\" button to replace all tabs by spaces."] = "Здається, що ви додали символи табуляції в цей файл. Будь-ласка, замініть кожен з них одним пробілом.<br>Підказка: Ви можете використовувати кнопку \"Переформатувати цей файл\" для заміни всіх символів табуляції пробілами.";
+  i18n["You aren't allowed to modify this file as it has already been modified by different user. Contact an administrator if you want to be able to modify it."] = "Вам недоступне редагування цього файла, оскільки він був змінений іншим користувачем. Зв'яжіться з адміністратором, якщо ви хочете змінити його.";
+  i18n["The patch you want to delete doesn't exist."] = "Патч, який ви хочете видалити, не існує.";
+  i18n["The patch you want to delete isn't owned by you. Only the author of the patch or a global administrator can delete it."] = "Патч, який ви хочете видалити, вам не належить. Тільки автор патча або глобальний адміністратор можуть видалити його.";
+  i18n["This action is available to global administrators only."] = "Ця дія доступна лише глобальному адміністратору.";
+  i18n["This action is available to global administrators or to administrators for this language only."] = "Ця дія доступна лише глобальному адміністратору, або адміністратору цієї мови.";
+  i18n["The file for which you want to clear local changes isn't owned by you.<br>You can only perform this action on your own files."] = "Файл, в якому ви хочете очистити локальні зміни, вам не належить.<br>Ця дія доступна лише для ваших файлів.";
+
+// In file: ../../js/ui/component/CheckDoc.js
+  i18n["Double-click the cell to open the file selection"] = "Щоб відкрити вибрані файли, двічі клікніть по комірці";
+  i18n["Extension"] = "Розширення";
+  i18n["Not documented"] = "Недокументовано";
+  i18n["Old style"] = "Старий стиль";
+  i18n["Bad refsect1 order"] = "Поганий порядок refsect1";
+  i18n["No parameters"] = "Немає параметрів";
+  i18n["No return values"] = "Немає значень, що повертаються";
+  i18n["No examples"] = "Немає прикладів";
+  i18n["No errors section"] = "Немає розділу \"Errors\"";
+  i18n["No see also"] = "Немає розділу \"See also\"";
+  i18n["Refsect1 role error"] = "Помилка ролі Refsect1";
+  i18n["Files"] = "Файли";
+  i18n["Open all files"] = "Відкрити всі файли";
+  i18n["Open selected files"] = "Відкрити вибрані файли";
+  i18n["Please, wait..."] = "Будь-ласка, почекайте...";
+
+// In file: ../../js/ui/component/WorkTreeGrid.js
+  i18n["Modified by"] = "Змінено";
+  i18n["Date"] = "Дата";
+  i18n["Y-m-d, H:i"] = "d.m.Y, H:i";
+  i18n["Users"] = "Користувачі";
+  i18n["Last modified"] = "Останні зміни";
+  i18n["Estimated progress"] = "Прогрес";
+  i18n["{0}% complete"] = "{0}% завершено";
+  i18n["Set the progress..."] = "Встановіть прогрес...";
+  i18n["View diff"] = "Проглянути відмінність";
+  i18n["Download the diff as a patch"] = "Скачати відмінності у вигляді патча";
+  i18n["Clear this change"] = "Скасувати ці зміни";
+  i18n["Cancel this deletion"] = "Скасувати це видалення";
+  i18n["Submit all files for review in patch:"] = "Відправити всі файли для перегляду в патч:";
+  i18n["Submit all files in this directory in patch:"] = "Відправити всі файли в цій директорії в патч:";
+  i18n["Submit as patch for review in:"] = "Відправити як патч для перегляду в:";
+  i18n["Create a new patch"] = "Створити новий патч";
+  i18n["Commit..."] = "Коміт...";
+  i18n["...this file"] = "...цього файла";
+  i18n["...all files from this folder"] = "...всіх файлів з цієї папки";
+  i18n["...all files from this patch"] = "...всіх файлів з цього патча";
+  i18n["...all files modified by me"] = "...всіх файлів, змінених мною";
+  i18n["You have no patch currently. You must create one."] = "У вас поки що немає патчів. Потрібно створити.";
+  i18n["Diff for file: {0}"] = "Відмінності для файла: {0}";
+  i18n["Administrator menu"] = "Меню адміністратора";
+
+  
+// In file: ../../js/ui/component/PatchesTreeGrid.js
+  i18n["Edit the description of this patch"] = "Редагувати опис цього патча";
+  i18n["Delete this patch"] = "Видалити цей патч";
+  i18n["Back all files to work in progress module"] = "Перемістити всі файли до модуля '" + i18n["Work in progress"] + "'";
+  i18n["Back all this patch to work in progress module"] = "Перемістити цей патч до модуля '" + i18n["Work in progress"] + "'";
+  i18n["Back all this folder to work in progress module"] = "Перемістити цю папку до модуля '" + i18n["Work in progress"] + "'";
+  i18n["Back this file to work in progress module"] = "Перемістити цей файл до модуля '" + i18n["Work in progress"] + "'";
+  i18n["Creation date: "] = "Дата створення: ";
+  i18n["View unified diff"] = "Проглянути відмінності (unified diff)";
+  i18n["Download the unified diff as a patch"] = "Скачати відмінності (unified diff) у вигляді патча";
+
+
+// In file: ../../js/ui/component/ManagePatchPrompt.js
+  i18n["Create"] = "Створити";
+  i18n["Modify this patch description"] = "Змінити опис цього патча";
+  i18n["Patch name"] = "Ім'я патча";
+  i18n["Patch description"] = "Опис патча";
+  
