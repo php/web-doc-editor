d202c275f8bc023fd7427ca41f6d111c1f017ff9
Sergey Panteleev <sergey@php.net>
2020-03-13 11:48:50 +0300

    feat(compress): Change rules to compress
    The compression rules are not duplicated and set once

--- scripts/utils/compress.sh
+++ scripts/utils/compress.sh
@@ -4,10 +4,10 @@
 # YUI Compressor.
 
 # Project root directory
-PROJECT_ROOT=`CDPATH= cd -- "$(dirname -- "$0")/../.." && pwd -P`
+PROJECT_ROOT=$(CDPATH= cd -- "$(dirname -- "$0")/../.." && pwd -P)
 
 # Store current path to return later to it
-CURRENT_PATH=`pwd`
+CURRENT_PATH=$(pwd)
 
 # YUI Compressor
 COMPRESSOR="java -jar scripts/utils/yuicompressor.jar --verbose"
@@ -18,52 +18,21 @@ cd $PROJECT_ROOT
 $COMPRESSOR --type css -o themes/loading-min.css \
   themes/loading.css
 
-# JS files for login page
-$COMPRESSOR --type js -o js/login-all.js \
-  js/util.js \
-  js/ux/others/login_specific/*.js \
-  js/login.js
-
 # JS files for login page - debug
 cat \
   js/util.js \
   js/ux/others/login_specific/*.js \
   js/login.js \
-  > js/login-all-debug.js
+  >js/login-all-debug.js
+
+# JS files for login page
+$COMPRESSOR --type js -o js/login-all.js js/login-all-debug.js
 
 # CSS files for login page
 $COMPRESSOR --type css -o themes/login-all.css \
   themes/flags.css \
   js/ux/others/login_specific/*.css \
   themes/login.css
-
-# JS files for main page
-$COMPRESSOR --type js -o js/main-all.js \
-  js/ux/codemirror2/lib/codemirror.js \
-  js/ux/codemirror2/mode/clojure/clojure.js \
-  js/ux/codemirror2/mode/xml/xml.js \
-  js/ux/codemirror2/mode/javascript/javascript.js \
-  js/ux/codemirror2/mode/clike/clike.js \
-  js/ux/codemirror2/mode/xmlpure/xmlpure.js \
-  js/ux/codemirror2/mode/css/css.js \
-  js/ux/codemirror2/mode/php/php.js \
-  js/ux/codemirror2/mode/htmlmixed/htmlmixed.js \
-  js/ux/codemirror2/mode/rst/rst.js \
-  js/ux/codemirror2/lib/util/search.js \
-  js/ux/codemirror2/lib/util/searchcursor.js \
-  js/ux/codemirror2/lib/util/dialog.js \
-  js/util.js \
-  js/ux/extjsPowered/*.js \
-  js/ux/extjsPowered/treegrid/TreeGridSorter.js \
-  js/ux/extjsPowered/treegrid/TreeGridColumnResizer.js \
-  js/ux/extjsPowered/treegrid/TreeGridNodeUI.js \
-  js/ux/extjsPowered/treegrid/TreeGridLoader.js \
-  js/ux/extjsPowered/treegrid/TreeGridColumns.js \
-  js/ux/extjsPowered/treegrid/TreeGrid.js \
-  js/ux/others/main_specific/*.js \
-  js/ui/task/*.js \
-  js/ui/cmp/*.js \
-  js/main.js
 
 # JS files for main page - debug
 cat \
@@ -92,7 +61,10 @@ cat \
   js/ui/task/*.js \
   js/ui/cmp/*.js \
   js/main.js \
-  > js/main-all-debug.js
+  >js/main-all-debug.js
+
+# JS files for main page
+$COMPRESSOR --type js -o js/main-all.js js/main-all-debug.js
 
 # CSS files for main page
 $COMPRESSOR --type css -o themes/main-all.css \
